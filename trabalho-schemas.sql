aab894382ac893d9df0973fb4c73dddc6419a201
João Freires <rmjoaovictor@gmail.com>
2019-11-16 01:02:47 -0300

    add address fields on client table

--- trabalho-schemas.sql
+++ trabalho-schemas.sql
@@ -13,22 +13,31 @@ CREATE TABLE data (
     dia_da_semana INT NOT NULL,
     fim_de_semana BOOLEAN NOT NULL,
     PRIMARY KEY (data_id)
-
 );
 
 
 CREATE TABLE cliente (
     cliente_id INT NOT NULL,
-    PRIMARY KEY (cliente_id)
+    PRIMARY KEY (cliente_id),
+    endereco VARCHAR(50) NOT NULL,
+    endereco2 VARCHAR(50) DEFAULT NULL,
+    distrito VARCHAR(20) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    pais VARCHAR(50) NOT NULL,
+    codigo_postal VARCHAR(10) DEFAULT NULL,
+    telefone VARCHAR(20) NOT NULL
 );
+
+
 CREATE TABLE funcionario (
     funcionario_id INT NOT NULL,
     eh_gerente BOOLEAN NOT NULL,
     PRIMARY KEY (funcionario_id)
 );
+
+
 CREATE TABLE loja (
     loja_id INT NOT NULL,
-   
     PRIMARY KEY (loja_id)
 );
 
@@ -40,7 +49,6 @@ CREATE TABLE filme (
     lingua_falada VARCHAR(50) NOT NULL,
     PRIMARY KEY (filme_id)
 );
-
 
 
 CREATE TABLE aluguel (
